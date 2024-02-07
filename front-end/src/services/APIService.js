4a987e8d7515260335aaee5fff8d9389e0f7ed4e
Samuel Pires <spires@ipn.pt>
2024-02-07 14:37:34 +0000

    API Request Secure

--- front-end/src/services/APIService.js
+++ front-end/src/services/APIService.js
@@ -8,7 +8,7 @@ const APIService = {
         return response.json();
     },
     getWeather: async (day) => {
-        const response = await fetch('http://api.ipma.pt/open-data/forecast/meteorology/cities/daily/hp-daily-forecast-day' + day + '.json');
+        const response = await fetch('https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/hp-daily-forecast-day' + day + '.json');
         return response.json();
     },
 }
